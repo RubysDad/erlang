217c872b566036d8d3aefb05b503467b6102a75a
Mark Morales <marktravismorales@gmail.com>
2020-01-11 14:36:26 -0600

    Lists Comprehensions

--- mylists.erl
+++ mylists.erl
@@ -32,3 +32,19 @@ map(F, [H|T]) -> [F(H)|map(F, T)].
 %%  9> mylists:map(fun(X) -> X*X end, List).
 %%  [1,4,9,16,25]
 
+%%  list comprehensions: are expressions that create lists without having to use funs, maps, or filters.
+
+%%  1> L = [1, 2, 3, 4, 5].
+%%  [1,2,3,4,5]
+%%  3> [2*X || X <- L].
+%%  [2,4,6,8,10]
+
+%%  4> Buy = [{oranges, 4}, {newspaper, 1}, {apples, 10}, {pears, 3}, {milk, 3}].
+%%  [{oranges,4},{newspaper,1},{apples,10},{pears,3},{milk,3}]
+%%  5> [{Name, 2 * Number} || {Name, Number} <- Buy].
+%%  [{oranges,8},{newspaper,2},{apples,20},{pears,6},{milk,6}]
+
+%%  Note that the tuple {Name, Number} to the right side of the (||) sign is a pattern
+%%  that matches each of the elements in the list Buy. The tuple to the left side,
+%%  {Name, 2*Number}, is a constructor.
+
