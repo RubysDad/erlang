b62c763e7aa84c5c65bd406a8c38a89f7e4f9734
Mark Morales <marktravismorales@gmail.com>
2020-01-28 00:04:40 -0600

    Exceptions

--- lib_misc.erl
+++ lib_misc.erl
@@ -10,7 +10,12 @@
 -author("mark").
 
 %% API
--export([for/3, odds_and_evens2/1, count_characters/1]).
+-export([for/3,
+         odds_and_evens2/1,
+%%         count_characters/1,
+         sqrt/1,
+         demo/0,
+         generate_exception/1]).
 
 %% creating our own custom for loop
 
@@ -49,11 +54,43 @@ odds_and_evens_acc([H|T], Odds, Evens) ->
 %%  lib_misc:odds_and_evens2([1,2,3,4,5,6]).
 %%  {[1,3,5],[2,4,6]}
 
-count_characters(Str) ->
-  count_characters(Str, #{}).
+%%count_characters(Str) ->
+%%  count_characters(Str, #{}).
+%%
+%%count_characters([H|T], #{ H := N } = X) ->
+%%  count_characters(T, X#{ H := N + 1 });
+%%  count_characters([H|T], X) ->
+%%    count_characters(T, X#{ H => 1});
+%%    count_characters([], X) -> X.
 
-count_characters([H|T], #{ H := N } = X) ->
-  count_characters(T, X#{ H := N + 1 });
-  count_characters([H|T], X) ->
-    count_characters(T, X#{ H => 1});
-    count_characters([], X) -> X.
+%% Improving the quality of error messages:
+sqrt(X) when X < 0 ->
+  error({squareRootNegativeArgument, X});
+sqrt(X) ->
+  math:sqrt(X).
+
+%% To catch all errors:
+%% try Expr
+%% catch
+%%   _:_ -> ... Code to handle all exceptions
+%% end
+
+generate_exception(5) -> error(a).
+
+%% To get a stacktrace
+demo() ->
+  try generate_exception(5)
+  catch
+    error:X ->
+      {X, erlang:get_stacktrace()}
+  end.
+
+%%  3> lib_misc:demo().
+%%  {a,[{lib_misc,generate_exception,1,
+%%  [{file,"lib_misc.erl"},{line,78}]},
+%%  {lib_misc,demo,0,[{file,"lib_misc.erl"},{line,81}]},
+%%  {erl_eval,do_apply,6,[{file,"erl_eval.erl"},{line,684}]},
+%%  {shell,exprs,7,[{file,"shell.erl"},{line,686}]},
+%%  {shell,eval_exprs,7,[{file,"shell.erl"},{line,642}]},
+%%  {shell,eval_loop,3,[{file,"shell.erl"},{line,627}]}]}
+
